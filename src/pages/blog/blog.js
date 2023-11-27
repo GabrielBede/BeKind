4ebcf42ea230621a3541d60da59703417a7dd01e
Bedê <bedegabriel14@gmail.com>
2023-11-26 22:34:59 -0300

    loja page

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './blog.css';
+import Noticias from './noticias.json';
+import Footer from '../../components/Footer/index';
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+  const [noticias, setNoticias] = useState([]);
+  const [noticiasExibidas, setNoticiasExibidas] = useState(0);
 
-export default Blog
+  useEffect(() => {
+    // Carrega notícias do arquivo JSON
+    setNoticias(Noticias);
+  }, []); // A dependência vazia garante que isso seja executado apenas uma vez ao montar o componente
+
+  const exibirNoticias = () => {
+    setNoticiasExibidas(noticiasExibidas + 2);
+  };
+
+  return (
+    <div class="conteudo">
+      <h2>Retratos das vidas nas calçadas</h2>
+      <div id="noticias-container">
+        {/* Renderizar notícias */}
+        {noticias.slice(0, noticiasExibidas).map(noticia => (
+          <div key={noticia.id} className="post">
+            <img src={noticia.imagem} alt={`Imagem da notícia ${noticia.id}`} />
+            <p>
+              <b>{noticia.titulo}</b><br />
+              {noticia.conteudo}
+            </p>
+            <a href={noticia.link} className="btn-artigo">Ler artigo</a>
+          </div>
+        ))}
+      </div>
+      <button id="btn-vermais" onClick={exibirNoticias}>
+        Ver mais...
+      </button>
+
+      <Footer/>
+    </div>
+  );
+};
+
+export default Blog;
+
+
--- src/pages/loja/loja.js
+++ src/pages/loja/loja.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from 'react';
+
+import ProdutoEco from '../../img/Produto-ECO.png';
+import ProdutoCaneca from '../../img/Produto-Caneca.png';
+import ProdutoCamisaBranca from '../../img/Produto-CamisaBranca.png';
+import ProdutoCamisaPreta from '../../img/Produto-CamisaPreta.png';
+import ProdutoAdesivos from '../../img/Produto-Adesivo.png';
+import ProdutoPoster from '../../img/Produto-Poster.png';
+
+import'./loja.css';
+import Footer from '../../components/Footer';
 
 const Loja = () => {
   return (
-    <div>Loja</div>
+    <div class="conteudo">
+
+      <h2>Nossos Produtos</h2>
+
+      <section className="flex">
+        <div>
+          <a href="/">
+            <img src={ProdutoEco} alt="" draggable="false"/>
+              <p className='loja-p'>Sacola ECO - BeKind</p>
+              <small>R$34,99</small>
+          </a>
+        </div>
+        <div>
+          <a href="/">
+            <img src={ProdutoCaneca} alt="" draggable="false"/>
+              <p className='loja-p'>Caneca - BeKind</p>
+              <small>R$14,99</small>
+          </a>
+        </div>
+        <div>
+          <a href="/">
+            <img src={ProdutoCamisaBranca} alt="" draggable="false"/>
+              <p className='loja-p'>T-Shirt: Camisa Branca - BeKind</p>
+              <small>R$39,99</small>
+          </a>
+        </div>
+        <div>
+          <a href="/">
+            <img src={ProdutoCamisaPreta} alt="" draggable="false"/>
+              <p className='loja-p'>T-Shirt: Camisa Preta - BeKind</p>
+              <small>R$39,99</small>
+          </a>
+        </div>
+        <div>
+          <a href="/">
+            <img src={ProdutoAdesivos} alt="" draggable="false"/>
+              <p className='loja-p'>Adesivos - BeKind</p>
+              <small>R$4,99</small>
+          </a>
+        </div>
+        <div>
+          <a href="/">
+            <img src={ProdutoPoster} alt="" draggable="false"/>
+              <p className='loja-p'>Poster - BeKind</p>
+              <small>R$6,99</small>
+          </a>
+        </div>
+      </section>
+
+      <Footer/>
+    </div>
   )
 }
 
-export default Loja
+export default Loja;
